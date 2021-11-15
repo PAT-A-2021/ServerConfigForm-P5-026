cbfc72440c1bd36e1cf02667f1c482888b7a694f
Muhammad Risqullah <m.risqullah.ft19@mail.umy.ac.id>
2021-11-15 11:36:43 +0700

    Add project files.

---
+++ ServerConfigForm-P5-026/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using ServiceMtk_P1_20190140026;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServerConfigForm_P5_026
+{
+    public partial class Form1 : Form
+    {
+        private ServiceHost Host;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Host = new ServiceHost(typeof(Matematika));
+            labelOnOrOff.Text = "Server on";
+            labelKet.Text = "Klik off untuk mematikan server";
+            Host.Open();
+            buttonOn.Enabled = false;
+            buttonOff.Enabled = true;
+        }
+
+        private void buttonOff_Click(object sender, EventArgs e)
+        {
+            buttonOn.Enabled = true;
+            buttonOff.Enabled = false;
+            Host.Close();
+            labelOnOrOff.Text = "Server off";
+            labelKet.Text = "Klik ON untuk menjalankan server";
+        }
+    }
+}
